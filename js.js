cf8d6a486dab88e43db1b416299f4b60b29034be
joffin5 <joffinshaji011.com>
2021-03-11 21:46:54 -0500

    edition

--- js.js
+++ js.js
@@ -3,9 +3,3 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const amount = document.getElementById('amount');
 
-const movieselect = document.getElementById('film');
-let ticketprice = +movieselect.Value;
-
-const populateUI=()=>{
-    const
-}
