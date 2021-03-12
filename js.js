8f41e9e77d0d2327e66bb8c7715cb9f3029ee8cb
joffin5 <joffinshaji011.com>
2021-03-11 21:38:22 -0500

    edition

---
+++ js.js
@@ -0,0 +1,11 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const amount = document.getElementById('amount');
+
+const movieselect = document.getElementById('film');
+let ticketprice = +movieselect.Value;
+
+const populateUI=()=>{
+    const
+}
